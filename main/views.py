7e00728cad6e7bf83ac877d2506fc27b7ef90e1d
Yash Patel <mailforyash04@gmail.com>
2022-03-29 22:25:51 +0530

    change in home page design

--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
+from .forms import SignUpUserForm
 
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
+
+class SignUp():
+    form_class = SignUpUserForm
+    template_class = 'main/register.html'
+    success_url = 'home'
