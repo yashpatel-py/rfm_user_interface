a70602001de6c69fa9b3fa1121850ccdaa689ea1
Yash Patel <mailforyash04@gmail.com>
2022-04-19 20:18:51 +0530

    some small change

--- rfm/settings.py
+++ rfm/settings.py
@@ -128,6 +128,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MESSAGE_TAGS = {
